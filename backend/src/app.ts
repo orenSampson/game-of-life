d80863b245c43754893fc43d8f417bd78d76fabe
orenSampson <oren.sampson@gmail.com>
2022-10-17 02:38:52 +0300

    front - basic setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,90 @@
 import express, { Request, Response, NextFunction } from "express";
-import { json } from "body-parser";
+import { Socket, Server } from "socket.io";
+// import { json } from "body-parser";
 
 import actionsRoutes from "./routes/actions";
+import { GameOfLife, cellState } from "./models/gameOfLife";
 
 const app = express();
 
-app.use(json());
+// app.use(json());
 
-app.use("/api", actionsRoutes);
+const gameOfLifeInstance = GameOfLife.getInstance();
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ message: err.message });
+gameOfLifeInstance.initBoardAction([
+  [
+    cellState.dead,
+    cellState.dead,
+    cellState.alive,
+    cellState.dead,
+    cellState.dead,
+  ],
+  [
+    cellState.dead,
+    cellState.dead,
+    cellState.dead,
+    cellState.alive,
+    cellState.dead,
+  ],
+  [
+    cellState.dead,
+    cellState.alive,
+    cellState.alive,
+    cellState.alive,
+    cellState.dead,
+  ],
+  [
+    cellState.dead,
+    cellState.dead,
+    cellState.dead,
+    cellState.dead,
+    cellState.dead,
+  ],
+  [
+    cellState.dead,
+    cellState.dead,
+    cellState.dead,
+    cellState.dead,
+    cellState.dead,
+  ],
+]);
+
+// gameOfLifeInstance.startAction();
+
+// gameOfLifeInstance.printBoard();
+
+// gameOfLifeInstance.nextStepAction();
+
+// gameOfLifeInstance.printBoard();
+
+// gameOfLifeInstance.nextStepAction();
+
+// gameOfLifeInstance.printBoard();
+
+// gameOfLifeInstance.resetAction();
+
+// gameOfLifeInstance.printBoard();
+
+// app.use("/api", actionsRoutes);
+
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+//   res.status(500).json({ message: err.message });
+// });
+
+const server = app.listen(8080);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
-app.listen(8080);
+const startListeners = (socket: Socket) => {
+  socket.on("init", (data) => {
+    console.log("init called");
+    console.log("data :>> ", data);
+  });
+
+  console.log("Client connected");
+};
+
+io.on("connection", startListeners);
